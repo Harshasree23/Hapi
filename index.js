dc616b36d7d0a3b0d9bd48ed57d2c4d52a101e0b
Sree Harsha <141744505+Harshasree23@users.noreply.github.com>
2024-09-03 21:35:02 +0530

    Added favicon

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.set('view engine','ejs');
 app.set('views',path.resolve('./views'));
 app.use(express.urlencoded( { extended : false } ));
 app.use(express.json());
-
+app.use("/public", express.static('public')); 
 
 // Routes
 app.use('/api',apiRoute);
