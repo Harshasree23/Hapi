9f31a3fa2435302b2d2687369a3b137d79e62215
Sree Harsha <141744505+Harshasree23@users.noreply.github.com>
2024-12-11 20:06:07 +0530

    Moved the functions of multer from user -> api

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,35 @@ const { handleGetApicontact, handlePostApicontact } = require('../controllers/co
 const { handleGetApiachievement, handlePostApiachievement } = require('../controllers/achievements');
 
 
+const multer = require('multer');
+const path = require('path');
+
+
+// Configure storage for uploaded files
+const storage = multer.diskStorage({
+    destination: './uploads/projects',
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+// Multer instance
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB max size per file
+    fileFilter: (req, file, cb) => {
+        const fileTypes = /jpeg|jpg|png/;
+        const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+        const mimetype = fileTypes.test(file.mimetype);
+
+        if (mimetype && extname) {
+            cb(null, true);
+        } else {
+            cb(new Error('Only images (jpeg, jpg, png) are allowed!'));
+        }
+    },
+});
+
 const apiRoute = express.Router();
 
 
